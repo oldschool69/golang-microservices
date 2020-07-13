2da7c2ddd6db8b71d71fe07ed348877504aa0e86
oldschool69 <flavio.marcondes@gmail.com>
2020-07-13 15:04:08 -0300

    Create create repo request structures

---
+++ src/api/domain/github/create_repo_test.go
@@ -0,0 +1,36 @@
+package github
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateRepoRequestAsJson(t *testing.T) {
+	request := CreateRepoRequest{
+		Name: "golang introduction",
+		Description: "a golang introduction repository",
+		Homepage: "https://github.com",
+		Private: true,
+		HasIssues: true,
+		HasProjects: true,
+		HasWiki: true,
+	}
+
+	// Test marshalling
+	bytes, err := json.Marshal(request)
+	assert.Nil(t, err)
+	assert.NotNil(t, bytes)
+	fmt.Println(string(bytes))
+
+
+	// Test unmarshalling
+	var target CreateRepoRequest
+	err = json.Unmarshal(bytes, &target)
+	assert.Nil(t, err)
+
+	assert.EqualValues(t, target.Name, request.Name)
+	assert.EqualValues(t, target.HasIssues, request.HasIssues)
+
+}
