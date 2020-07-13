383e7565beb360b4fecd4e5630596d025fdd7c3a
oldschool69 <flavio.marcondes@gmail.com>
2020-07-13 20:51:26 -0300

    Create githubdomain

---
+++ src/api/providers/github_provider/github_provider.go
@@ -0,0 +1,67 @@
+package github_provider
+
+import (
+	"encoding/json"
+	"fmt"
+	"golang-microservices/src/api/clients/restclient"
+	"golang-microservices/src/api/domain/github"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const (
+	headerAuthorization = "Authorization"
+	headerAuthorizationFormat = "token %s"
+	urlCreateRepo = "https://api.github.com/user/repos"
+)
+
+func getAuthorizationHeader(accessToken string) string {
+	return fmt.Sprintf(headerAuthorizationFormat, accessToken)
+}
+
+func CreateRepo(accessToken string, request github.CreateRepoRequest) (*github.CreateRepoResponse, *github.GithubErrorResponse) {
+	headers := http.Header{}
+	headers.Set(headerAuthorization, getAuthorizationHeader(accessToken))
+	response, err := restclient.Post(urlCreateRepo, request, headers)
+	if err != nil {
+		log.Printf("Error when trying to create new repo on github: %s\n", err.Error())
+		return nil, &github.GithubErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message: err.Error(),
+		}
+	}
+
+	bytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, &github.GithubErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message: "Invalid response body",
+		}
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode > 299 {
+		var errResponse github.GithubErrorResponse
+		if err := json.Unmarshal(bytes, &errResponse); err != nil {
+			return nil, &github.GithubErrorResponse{
+				StatusCode: http.StatusInternalServerError,
+				Message: "Invalid json response body",
+			}
+		}
+		errResponse.StatusCode = response.StatusCode
+		return nil, &errResponse
+
+	}
+	var result github.CreateRepoResponse
+	if err := json.Unmarshal(bytes, &result); err != nil {
+		log.Printf("Error when trying to unmarshal create repo successful response: %s\n", err.Error())
+		return nil, &github.GithubErrorResponse{
+			StatusCode: http.StatusInternalServerError,
+			Message: "Error unmarshalling github create repo response",
+		}
+	}
+
+	return &result, nil
+}
